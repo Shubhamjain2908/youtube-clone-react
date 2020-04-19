d62a8a5c078725e9c23d8604f2151ea1b4806d43
Shubham Jain <shubhamjain2908@gmail.com>
2020-04-20 00:44:53 +0530

    handleChange & handleSubbmit event on SearchBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
+import youtube from './api/youtube';
 import { SearchBar, VideoDetails, VideoList } from './components/index';
-// import youtube from './api/youtube';
 
-const App = () => {
-  return (
-    <Grid justify="centre" container spacing={16}>
-      <Grid item xs={12}>
-        <Grid container spacing={16}>
-          <Grid item xs={12}>
-            <SearchBar />
-          </Grid>
-          <Grid item xs={8}>
-            <VideoList />
-          </Grid>
-          <Grid item xs={4}>
-            <VideoDetails />
+class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', { params: { q: searchTerm } });
+    console.log('res=> ', response);
+  }
+
+  render() {
+    return (
+      <Grid container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetails />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { Paper, TextField } from '@material-ui/core';
+import React from 'react';
 
-const SearchBar = () => {
-    return (
-        <h1>
-            Search Bar
-        </h1>
-    )
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: ''
+    }
+
+    handleChange = (event) => this.setState({ searchTerm: event.target.value });
+
+    handleSubmit = (event) => {
+        console.log('EV => ', event);
+        event.peventDefault();
+        const { searchTerm } = this.state;
+        const { onFormSubmit } = this.props;
+        onFormSubmit(searchTerm);
+    }
+
+    render() {
+        return (
+            <Paper elevation={6} style={{ padding: '25px' }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField label="Search..." onChange={this.handleChange}></TextField>
+                </form>
+            </Paper>
+        )
+    }
 }
 
-export default SearchBar
+export default SearchBar;
