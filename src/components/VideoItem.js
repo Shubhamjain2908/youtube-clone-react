1be18c1f334878c07d88e49352a5ea843b0a0081
Shubham Jain <shubhamjain2908@gmail.com>
2020-04-20 02:24:12 +0530

    clicking videos from VIdeoList

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,11 @@
 import { Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     const { snippet } = video;
     return (
         <Grid item xs={12}>
-            <Paper style={{ display: 'flex', alignItems: 'centre' }}>
+            <Paper style={{ display: 'flex', alignItems: 'centre', cursor: 'pointer' }} onClick={() => onVideoSelect(video)}>
                 <img style={{ marginRight: '20px' }} alt="thumbnail" src={snippet.thumbnails.medium.url} />
                 <Typography variant="subtitle1"><b>{snippet.title}</b></Typography>
             </Paper>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,9 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
-    const listOfVideos = videos.map((video, i) => <VideoItem key={i} video={video} />);
+const VideoList = ({ videos, onVideoSelect }) => {
+    const listOfVideos = videos.map((video, i) => <VideoItem onVideoSelect={onVideoSelect} key={i} video={video} />);
+
     return (
         <Grid container spacing={10}>
             {listOfVideos}
