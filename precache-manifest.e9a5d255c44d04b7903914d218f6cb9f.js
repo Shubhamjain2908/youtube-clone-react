ef1fa6c7df4f5918783e49095ace624334d9a056
Shubham Jain <Shubhamjain2908@gmail.com>
2020-04-27 20:00:09 +0530

    Updates

---
+++ precache-manifest.e9a5d255c44d04b7903914d218f6cb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa2dd06640ed9789c1dd531e9bbd8ac",
+    "url": "/youtube-clone-react/index.html"
+  },
+  {
+    "revision": "a54c95a2598b35a4b774",
+    "url": "/youtube-clone-react/static/js/2.a95a51e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone-react/static/js/2.a95a51e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d99bd989f55367edfd",
+    "url": "/youtube-clone-react/static/js/main.113a99e5.chunk.js"
+  },
+  {
+    "revision": "5723db48b90959d7e14a",
+    "url": "/youtube-clone-react/static/js/runtime-main.668f3ee9.js"
+  }
+]);
